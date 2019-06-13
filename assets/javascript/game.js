591af9db648f471ab9bb8c3633d49e6d667dbb73
milee8989 <milee8989@gmail.com>
2019-06-13 15:53:25 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,18 @@
 var crystal = {
-    crystal1:0,
-    crystal2:0,
-    crystal3:0,
-    crystal4:0,
-    }; 
+    crystal1: 0,
+    crystal2: 0,
+    crystal3: 0,
+    crystal4: 0,
+};
 
-    
+
 
 
 
 
 var currentNumber = 0;
-var targetNumber = Math.floor(Math.random()* 120) + 10;
-var counter = 0 ;
+var targetNumber = Math.floor(Math.random() * 120) + 10;
+var counter = 0;
 var wincount = 0;
 var lostcount = 0;
 
@@ -23,110 +23,108 @@ var lostcount = 0;
 
 ////////////////////////////////////////////////////
 
-    var getRandom = function(min, max){
-        return Math.floor(Math.random() * (max) + (min));
-            }
+var getRandom = function (min, max) {
+    return Math.floor(Math.random() * (max) + (min));
+}
 
-    //var randomValue = Math.floor(Math.random() * (12)) + 1;
-    //console.log(randomValue);
-    ///////////////////////////////////////////////////////
-    var startGame= function(){
-    
-        //reset the curretn score
-        currentNumber = 0;
-        //set a new target score
-        targetNumber = getRandom(19, 120);
+//var randomValue = Math.floor(Math.random() * (12)) + 1;
+//console.log(randomValue);
+///////////////////////////////////////////////////////
+var startGame = function () {
 
-        // set different values for each of the crystals
-        crystal.crystal1     = getRandom(1,12)
-        crystal.crystal2    = getRandom(1,12)
-        crystal.crystal3    = getRandom(1,12)
-        crystal.crystal4   = getRandom(1,12)
+    //reset the curretn score
+    currentNumber = 0;
+    //set a new target score
+    targetNumber = getRandom(19, 102);
 
-        $("#yourNumber").html(currentNumber);
-        $("#targetNumber").html(targetNumber);
+    // set different values for each of the crystals
+    crystal.crystal1 = getRandom(1, 12)
+    crystal.crystal2 = getRandom(1, 12)
+    crystal.crystal3 = getRandom(1, 12)
+    crystal.crystal4 = getRandom(1, 12)
 
-       
-        
-        
-    }
-
-    $(".crystal-image").on("click", function() {
-
-        var crystalId = $(this).attr("id");
-
-        currentNumber = currentNumber + crystal[crystalId];
-        $("#yourNumber").html(currentNumber);
-        checkwin();
-        console.log("value" + crystalId);
-    })
-    
+    $("#yourNumber").html(currentNumber);
+    $("#targetNumber").html(targetNumber);
+    $("#winCount").html(wincount);
+    $("#lossCount").html(lostcount);
 
 
-    var checkwin = function(){
-        if(currentNumber > targetNumber){
-            alert("you lost");
-            lostcount++;
-            $("#losscount").html(lostcount);
-            //var answer=confirm("you lost, do you wanna play again?");
-           reset();
-            
-            // if (answer){
-            //     startGame();
-            // }
-            
-        }
 
-    
-        else if (yourNumber == targetNumber){
-            alert("you won");
-            wincount++;
-            $("#wincount").html(wincount);
-           // var answer=confirm("you won");
-            reset();
+}
 
-            // if (answer){
-            //     startGame();
-            // }
-        }
-           
+$(".crystal-image").on("click", function () {
+
+    var crystalId = $(this).attr("id");
+
+    currentNumber = currentNumber + crystal[crystalId];
+    $("#yourNumber").html(currentNumber);
+    checkwin();
+    console.log("value" + crystalId);
+})
+
+
+
+var checkwin = function () {
+    if (currentNumber > targetNumber) {
+        alert("you lost");
+        lostcount++;
+        $("#losscount").html(lostcount);
+        //var answer=confirm("you lost, do you wanna play again?");
+        startGame();
+
 
 
     }
 
-    function reset(){
 
-        var getRandom = function(min, max){
-            return Math.floor(Math.random() * (max) + (min));}
-            $("#targetNumber").text(targetNumber);
+    else if (currentNumber == targetNumber) {
+        alert("you won");
+        wincount++;
+        $("#wincount").html(wincount);
+        // var answer=confirm("you won");
+        startGame();
+
+    }
+
+
+
+}
+
+function reset() {
+
+    currentNumber = 0;
+
+    crystal.crystal1 = getRandom(1, 12)
+    crystal.crystal2 = getRandom(1, 12)
+    crystal.crystal3 = getRandom(1, 12)
+    crystal.crystal4 = getRandom(1, 12)
     
-        crystal.crystal1     = getRandom(1,12)
-        crystal.crystal2    = getRandom(1,12)
-        crystal.crystal3    = getRandom(1,12)
-        crystal.crystal4   = getRandom(1,12)
-        currentNumber= 0;
-        $("#yourNumber").text(currentNumber);
+   
+    $("#yourNumber").text(currentNumber);
+    $("#targetNumber").text(targetNumber);
+    $("#winCount").html(wincount);
+    $("#lossCount").html(lostcount);
 
-        }
-    
-
-    
+}
 
 
-    // $("#crystal1").click(function () {
-        
-    //     addValues(crystal.crystal1);
-    // });
-    // $("#crystal2").click(function () {
-    //     addValues(crystal.crystal2);
-    // });
-    // $("#crystal3").click(function () {
-    //     addValues(crystal.crystal3);
-    // });
-    // $("#crystal4").click(function () {
-    //     addValues(crystal.crystal4);
-    // });
 
 
-    
+
+// $("#crystal1").click(function () {
+
+//     addValues(crystal.crystal1);
+// });
+// $("#crystal2").click(function () {
+//     addValues(crystal.crystal2);
+// });
+// $("#crystal3").click(function () {
+//     addValues(crystal.crystal3);
+// });
+// $("#crystal4").click(function () {
+//     addValues(crystal.crystal4);
+// });
+
+
+
 startGame();
